61ffeda3b431deb7d6f6510870f34dcde792fcfc
jayofstella <jayofstella@qq.com>
2026-08-27 14:03:41 +0800

    Bump FineVolume to v0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.finevolume"
         minSdk = 30
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.4.1"
+        versionCode = 11
+        versionName = "0.4.2"
     }
 
     compileOptions {
