504f6d391434fc68be73a66a0d4db5003f8ab616
jayofstella <jayofstella@qq.com>
2026-08-28 18:04:26 +0800

    FineVolume v0.11.0: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 android { namespace="com.finevolume"; compileSdk=35
-    defaultConfig { applicationId="com.finevolume"; minSdk=30; targetSdk=35; versionCode=19; versionName="0.10.0" }
+    defaultConfig { applicationId="com.finevolume"; minSdk=30; targetSdk=35; versionCode=20; versionName="0.11.0" }
     compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget="17" }
 }
